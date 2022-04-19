71beb0f0dcf91f09e552c735d3d36077cd18e296
Robert Greene <rtgreene@outlook.com>
2022-04-18 22:39:39 -0400

    working on submission mechanics for post

--- answer-submission.php
+++ answer-submission.php
@@ -1,3 +1,16 @@
 <?php
-echo $_POST['answer'];
-echo $_POST['question-num'];
+
+include('db.php');
+
+$answer_submit_query = "INSERT INTO post    (author, body, question_id)
+                        VALUES              ('me',   ";
+$answer_submit_query .= $_POST['answer'];
+$answer_submit_query .= ', ';
+$answer_submit_query .= $_POST['question-num'];
+$answer_submit_query .= ');';
+
+$answer_submit_result = mysqli_query($connection, $answer_submit_query);
+
+echo $answer_submit_query;
+echo '<br>';
+echo $answer_submit_result;
